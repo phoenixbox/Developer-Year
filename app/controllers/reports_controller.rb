8f4914ace1fb0695696757fce952d6dbb6864fc6
Shane Rogers <srogers@quickleft.com>
2014-12-12 18:54:16 -0800

    need 2 factor aith on the app

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,9 @@
+class ReportsController < ApplicationController
+  def create
+    binding.pry
+    # RESTART
+    # Two factor authentication errors from the command line
+    # Unsure if I can make these 2factor requests from the GITHUB wrapper lib??
+    # https://developer.github.com/v3/auth/#working-with-two-factor-authentication
+  end
+end
